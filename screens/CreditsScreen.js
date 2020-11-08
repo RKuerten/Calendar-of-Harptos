1ea8488b7e107818c1e8e9d49ad6c4b87dc17683
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-08 18:28:53 -0300

    Finished Credits screen

--- screens/CreditsScreen.js
+++ screens/CreditsScreen.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Container, Icon } from "native-base";
+import { Container } from "native-base";
 
-import { Content } from "../components";
+import { Content, Header } from "../components";
 import Colors from "../constants/Colors";
+import Theme from "../utils/Theme";
 
 export default class CreditsScreen extends React.Component {
-  state = {};
-
   render() {
     let { navigation } = this.props;
 
     return (
       <Container>
+        <Header navigation={navigation} title="Créditos" />
         <Content>
-          <View style={styles.fullView}>
-            <Text>Credits</Text>
+          <View style={styles.container}>
+            <Text style={styles.text}>
+              Forgotten Realms is a trademark of Wizards of the Coast, Inc., a
+              subsidiary of Hasbro, Inc. All Rights Reserved.
+              {"\n\n"}
+              Source code is © 2020 by Rodrigo Kuerten.
+            </Text>
           </View>
         </Content>
       </Container>
@@ -24,9 +29,14 @@ export default class CreditsScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  fullView: {
+  container: {
     justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
+    flex: 1,
+    padding: 20,
+  },
+  text: {
+    color: Colors.black,
+    fontFamily: "Roboto_medium",
+    fontSize: Theme.responsiveFontSize(15),
   },
 });
