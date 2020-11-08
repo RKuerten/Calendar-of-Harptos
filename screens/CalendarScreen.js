c34ed2e4d40a2a94574c646449848703d7569bfc
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-08 17:15:10 -0300

    Added Fab to Calendar screen

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,20 +1,49 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Container, Icon } from "native-base";
+import { FAB, Portal } from "react-native-paper";
+import { useIsFocused } from "@react-navigation/native";
+import { Container } from "native-base";
 
-import { Content } from "../components";
+import { Content, Header } from "../components";
 import Colors from "../constants/Colors";
 
-export default class CalendarScreen extends React.Component {
-  state = {};
+class CalendarClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
 
   render() {
-    let { navigation } = this.props;
+    let { isFocused, navigation } = this.props;
+    let { open } = this.state;
 
     return (
       <Container>
+        <Header title="The Year of the Phaerimm's Vengeance" />
+        <Portal>
+          <FAB.Group
+            actions={[
+              {
+                icon: "copyright",
+                label: "Créditos",
+                onPress: () => navigation.navigate("Credits"),
+              },
+              {
+                icon: "bell",
+                label: "Remind",
+                onPress: () => console.log("Pressed notifications"),
+              },
+            ]}
+            icon="plus"
+            open={open}
+            onStateChange={() => this.setState({ open: !this.state.open })}
+            visible={isFocused}
+          />
+        </Portal>
         <Content>
-          <View style={styles.fullView}>
+          <View style={styles.container}>
             <Text>Initial version</Text>
           </View>
         </Content>
@@ -23,8 +52,13 @@ export default class CalendarScreen extends React.Component {
   }
 }
 
+export default function CalendarScreen(props) {
+  const isFocused = useIsFocused();
+  return <CalendarClass {...props} isFocused={isFocused} />;
+}
+
 const styles = StyleSheet.create({
-  fullView: {
+  container: {
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 20,
