5a63c2d6670e2591fbc883a3c2f1689da9b900f9
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-09 23:18:39 -0300

    Changed months.json to Months.js

---
+++ data/Months.js
@@ -0,0 +1,74 @@
+export const months = [
+  {
+    name: "Hammer",
+    holiday: "Midwinter",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Alturiak",
+    holiday: "",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Ches",
+    holiday: "",
+    event: "Ches 19: Spring Equinox",
+    leap: false,
+  },
+  {
+    name: "Tarsakh",
+    holiday: "Greengrass",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Mirtul",
+    holiday: "",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Kythorn",
+    holiday: "",
+    event: "Kythorn 20: Summer Solstice",
+    leap: false,
+  },
+  {
+    name: "Flamerule",
+    holiday: "Midsummer",
+    event: "",
+    leap: true,
+  },
+  {
+    name: "Eleasis",
+    holiday: "",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Eleint",
+    holiday: "Highharvestide",
+    event: "Eleint 21: Autumn Equinox",
+    leap: false,
+  },
+  {
+    name: "Marpenoth",
+    holiday: "",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Uktar",
+    holiday: "Feast of the Moon",
+    event: "",
+    leap: false,
+  },
+  {
+    name: "Nightal",
+    holiday: "",
+    event: "Nightal 20: Winter Equinox",
+    leap: false,
+  },
+];
