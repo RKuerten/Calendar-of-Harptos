9c2aa1fb512172ab773267b6d8ef013053fb4c2c
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-10 20:25:07 -0300

    Updated year and month switches

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -4,20 +4,44 @@ import { useIsFocused } from "@react-navigation/native";
 import { Container } from "native-base";
 
 import { months } from "../data/Months";
+import { years } from "../data/Years";
 import { CalendarSwitch, Content, Header, Month } from "../components";
+
+const yearsMax = years.length;
 
 class CalendarClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      fyear: { year: "", name: "" },
       month: 0,
+      year: 2296,
+      useFYear: false,
     };
   }
+
+  _getYearName = () => {
+    let { fyear, useFYear, year } = this.state;
+    if (useFYear) {
+      return fyear.name;
+    } else {
+      return years[year].name;
+    }
+  };
+
+  _getYearValue = () => {
+    let { fyear, useFYear, year } = this.state;
+    if (useFYear) {
+      return fyear.name;
+    } else {
+      return `${years[year].year} DR`;
+    }
+  };
 
   _handleMonthDown = () => {
     let { month } = this.state;
     if (month - 1 < 0) {
-      this.setState({ month: 11 }); //add -1 to year
+      this.setState({ month: 11 }, () => this._handleYearDown());
     } else {
       this.setState({ month: month - 1 });
     }
@@ -26,25 +50,80 @@ class CalendarClass extends React.Component {
   _handleMonthUp = () => {
     let { month } = this.state;
     if (month + 1 > 11) {
-      this.setState({ month: 0 }); //add +1 to year
+      this.setState({ month: 0 }, () => this._handleYearUp());
     } else {
       this.setState({ month: month + 1 });
     }
   };
 
+  _handleYearDown = () => {
+    let { fyear, useFYear, year } = this.state;
+    if (useFYear) {
+      if (fyear.year - 1 === 1600) {
+        this.setState({ year: 2296, useFYear: false });
+      } else {
+        this.setState({
+          fyear: {
+            year: fyear.year - 1,
+            name: `${fyear.year - 1} DR`,
+          },
+        });
+      }
+    } else if (year - 1 === -1) {
+      this.setState({
+        fyear: {
+          year: -701,
+          name: "-701 DR",
+        },
+        useFYear: true,
+      });
+    } else {
+      this.setState({ year: year - 1 });
+    }
+  };
+
+  _handleYearUp = () => {
+    let { fyear, useFYear, year } = this.state;
+    if (useFYear) {
+      if (fyear.year + 1 === -700) {
+        this.setState({ year: 0, useFYear: false });
+      } else {
+        this.setState({
+          fyear: {
+            year: fyear.year + 1,
+            name: `${fyear.year + 1} DR`,
+          },
+        });
+      }
+    } else if (year + 1 === yearsMax) {
+      this.setState({
+        fyear: {
+          year: 1601,
+          name: "1601 DR",
+        },
+        useFYear: true,
+      });
+    } else {
+      this.setState({ year: year + 1 });
+    }
+  };
+
   render() {
     let { navigation } = this.props;
-    let { month } = this.state;
+    let { month, year } = this.state;
+    const yearValue = this._getYearValue();
+    const yearName = this._getYearName();
 
     return (
       <Container>
-        <Header
-          navigation={navigation}
-          title="The Year of the Phaerimm's Vengeance"
-        />
+        <Header navigation={navigation} title={yearName} />
         <Content>
           <View style={styles.container}>
-            <CalendarSwitch title="Year" />
+            <CalendarSwitch
+              title={yearValue}
+              onPressLeft={() => this._handleYearDown()}
+              onPressRight={() => this._handleYearUp()}
+            />
             <CalendarSwitch
               onPressLeft={() => this._handleMonthDown()}
               onPressRight={() => this._handleMonthUp()}
