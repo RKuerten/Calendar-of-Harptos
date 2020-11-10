80ce17b03020edcb2d2bf65148d777740956879c
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-09 22:26:51 -0300

    Switched DefaultNavigation from Stack to TabNavigation

--- navigation/DefaultNavigation.js
+++ navigation/DefaultNavigation.js
@@ -1,18 +1,83 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "native-base";
 
+import Colors from "../constants/Colors";
 import CalendarScreen from "../screens/CalendarScreen";
 import CreditsScreen from "../screens/CreditsScreen";
+import SearchScreen from "../screens/SearchScreen";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const icons = {
+  Calendar: {
+    name: "calendar",
+    type: "MaterialCommunityIcons",
+  },
+  Search: {
+    name: "search",
+    type: "MaterialIcons",
+  },
+  Credits: {
+    name: "copyright",
+    type: "MaterialCommunityIcons",
+  },
+};
 
 const DefaultNavigation = () => {
   return (
-    <Stack.Navigator headerMode="none" initialRouteName="Calendar">
-      <Stack.Screen name="Calendar" component={CalendarScreen} />
-      <Stack.Screen name="Credits" component={CreditsScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      initialRouteName="Calendar"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          return (
+            <Icon
+              style={[styles.icon, focused && styles.iconActive]}
+              type={icons[route.name].type}
+              name={icons[route.name].name}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        style: {
+          alignItems: "center",
+          backgroundColor: Colors.tintColor,
+          height: 60,
+          justifyContent: "space-between",
+        },
+        activeTintColor: "#fff",
+        inactiveTintColor: Colors.tabIconDefault,
+        keyboardHidesTabBar: true,
+      }}
+    >
+      <Tab.Screen
+        name="Calendar"
+        options={{ title: "" }}
+        component={CalendarScreen}
+      />
+      <Tab.Screen
+        name="Search"
+        options={{ title: "" }}
+        component={SearchScreen}
+      />
+      <Tab.Screen
+        name="Credits"
+        options={{ title: "" }}
+        component={CreditsScreen}
+      />
+    </Tab.Navigator>
   );
 };
 
 export default DefaultNavigation;
+
+const styles = StyleSheet.create({
+  icon: {
+    color: "rgba(0, 0, 0, 0.40)",
+    fontSize: 30,
+    marginTop: 15,
+  },
+  iconActive: { color: Colors.black },
+});
