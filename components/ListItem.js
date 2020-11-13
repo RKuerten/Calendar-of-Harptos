d7bc0f689f665ef224f6979ff7be36d0523d49ca
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-12 21:47:11 -0300

    Created ListItem component

---
+++ components/ListItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+import Colors from "../constants/Colors";
+import Theme from "../utils/Theme";
+
+export default class ListItem extends React.PureComponent {
+  static defaultProps = {
+    onPress: () => {},
+    item: { year: "", name: "" },
+  };
+
+  render() {
+    let { item, onPress } = this.props;
+
+    return (
+      <TouchableOpacity 
+        style={styles.itemWrapper}
+        onPress={onPress}     
+      >
+        <Text style={styles.itemTitle}>{item.name}</Text>
+        <Text style={styles.itemDesc}>{`${item.year} DR`}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  itemWrapper: {
+    marginHorizontal: 10,
+    padding: 10,
+  },
+  itemTitle: {
+    color: Colors.black,
+    fontFamily: "Roboto_medium",
+    fontSize: Theme.responsiveFontSize(16),
+    marginVertical: 2,
+  },
+  itemDesc: {
+    color: "#666",
+    fontSize: Theme.responsiveFontSize(14),
+  },
+});
--- components/index.js
+++ components/index.js
@@ -2,8 +2,18 @@ import CalendarSwitch from "./CalendarSwitch";
 import Content from "./Content";
 import Drawer from "./Drawer";
 import Header from "./Header";
+import ListItem from "./ListItem";
 import Loading from "./Loading";
 import LunarMonth from "./LunarMonth";
 import Month from "./Month";
 
-export { CalendarSwitch, Content, Drawer, Header, Loading, LunarMonth, Month };
+export {
+  CalendarSwitch,
+  Content,
+  Drawer,
+  Header,
+  ListItem,
+  Loading,
+  LunarMonth,
+  Month,
+};
