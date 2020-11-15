a2fbf8ea325f5098bcb33e2e43ef3d3e87235a18
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-14 22:37:57 -0300

    Updated LunarCalendar screen with temporary placeholder credits

--- screens/LunarCalendarScreen.js
+++ screens/LunarCalendarScreen.js
@@ -1,178 +1,41 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { useIsFocused, useRoute } from "@react-navigation/native";
-import { Container } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
 
-import { months } from "../data/Months";
-import { phases } from "../data/MoonPhases";
-import { years } from "../data/Years";
-import { CalendarSwitch, Content, Header, LunarMonth } from "../components";
+import { Header } from "../components";
+import Colors from "../constants/Colors";
+import Theme from "../utils/Theme";
 
-const yearsMax = years.length; //2296
-
-class LunarCalendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fyear: { year: "", name: "" },
-      month: 0,
-      year: props.useSearchYear ? props.route.params.year : 2188, //1492 DR
-      useFYear: false,
-    };
-  }
-
-  _getYearLabel = () => {
-    let { fyear, useFYear, year } = this.state;
-    if (useFYear) {
-      return fyear.name;
-    } else {
-      return `${years[year].year} DR`;
-    }
-  };
-
-  _getYearName = () => {
-    let { fyear, useFYear, year } = this.state;
-    if (useFYear) {
-      return fyear.name;
-    } else {
-      return years[year].name;
-    }
-  };
-
-  _handleMonthDown = () => {
-    let { month } = this.state;
-    if (month - 1 < 0) {
-      this.setState({ month: 11 }, () => this._handleYearDown());
-    } else {
-      this.setState({ month: month - 1 });
-    }
-  };
-
-  _handleMonthUp = () => {
-    let { month } = this.state;
-    if (month + 1 > 11) {
-      this.setState({ month: 0 }, () => this._handleYearUp());
-    } else {
-      this.setState({ month: month + 1 });
-    }
-  };
-
-  _handleYearDown = () => {
-    let { fyear, useFYear, year } = this.state;
-    if (useFYear) {
-      if (fyear.year - 1 === 1600) {
-        this.setState({ year: 2296, useFYear: false });
-      } else {
-        this.setState({
-          fyear: {
-            year: fyear.year - 1,
-            name: `${fyear.year - 1} DR`,
-          },
-        });
-      }
-    } else if (year - 1 === -1) {
-      this.setState({
-        fyear: {
-          year: -701,
-          name: "-701 DR",
-        },
-        useFYear: true,
-      });
-    } else {
-      this.setState({ year: year - 1 });
-    }
-  };
-
-  _handleYearUp = () => {
-    let { fyear, useFYear, year } = this.state;
-    if (useFYear) {
-      if (fyear.year + 1 === -700) {
-        this.setState({ year: 0, useFYear: false });
-      } else {
-        this.setState({
-          fyear: {
-            year: fyear.year + 1,
-            name: `${fyear.year + 1} DR`,
-          },
-        });
-      }
-    } else if (year + 1 === yearsMax) {
-      this.setState({
-        fyear: {
-          year: 1601,
-          name: "1601 DR",
-        },
-        useFYear: true,
-      });
-    } else {
-      this.setState({ year: year + 1 });
-    }
-  };
-
-  _isLeapYear = () => {
-    let { fyear, useFYear, year } = this.state;
-    let currentYear = 0;
-    if (useFYear) {
-      currentYear = fyear.year;
-    } else {
-      currentYear = parseInt(years[year].year);
-    }
-    return Math.sqrt(Math.pow(currentYear % 4, 2));
-  };
-
+export default class LunarCalendarScreen extends React.Component {
   render() {
     let { navigation } = this.props;
-    let { month } = this.state;
-    const isLeapYear = this._isLeapYear();
-    const yearLabel = this._getYearLabel();
-    const yearName = this._getYearName();
 
     return (
-      <Container>
-        <Header navigation={navigation} title={yearName} />
-        <Content>
-          <View style={styles.container}>
-            <CalendarSwitch
-              onPressLeft={() => this._handleYearDown()}
-              onPressRight={() => this._handleYearUp()}
-              title={yearLabel}
-            />
-            <CalendarSwitch
-              onPressLeft={() => this._handleMonthDown()}
-              onPressRight={() => this._handleMonthUp()}
-              title={months[month].name}
-            />
-            <LunarMonth
-              isLeapYear={isLeapYear}
-              month={months[month]}
-              phases={phases[isLeapYear][month]}
-            />
-          </View>
-        </Content>
-      </Container>
+      <View style={styles.container}>
+        <Header navigation={navigation} title="LunarCalendar" />
+        <View style={styles.innerContainer}>
+          <Text style={styles.text}>
+            Forgotten Realms is a trademark of Wizards of the Coast, Inc., a
+            subsidiary of Hasbro, Inc. All Rights Reserved.
+            {"\n\n"}
+            Source code is © 2020 by Rodrigo Kuerten.
+          </Text>
+        </View>
+      </View>
     );
   }
-}
-
-export default function LunarCalendarScreen(props) {
-  const isFocused = useIsFocused();
-  const route = useRoute();
-  const useSearchYear = route.params != null;
-
-  return (
-    <LunarCalendar
-      {...props}
-      isFocused={isFocused}
-      useSearchYear={useSearchYear}
-    />
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  text: {
+    color: Colors.black,
+    fontFamily: "Roboto_medium",
+    fontSize: Theme.responsiveFontSize(15),
   },
 });
