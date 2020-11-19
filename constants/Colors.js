c61348ba7a873a7c3a7007055d6a919cba3ef854
Rodrigo Kuerten <rodkue@gmail.com>
2020-11-18 21:03:49 -0300

    Documented the Colors constants

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,13 +1,17 @@
 const tintColor = "#F0BE8F";
 
+/** The base colors of the app. */
 export default {
+  /** The app's main color: #F0BE8F */
   tintColor,
+  /** Black: #000 */
   black: "#000",
+  /** White: #FFF */
   white: "#FFF",
+  /** Dark gray: #666 */
   darkGray: "#666",
+  /** The basic background color: #F4F4F4 */
   background: "#F4F4F4",
+  /** Border color of various components: #DFDFDF */
   dayBorder: "#DFDFDF",
-  successBackground: "#6CB14B",
-  errorBackground: "red",
-  warningBackground: "#E5BA49",
 };
